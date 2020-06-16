6640c9ea85a73602314c5ff3da7adacd24238196
meara <mcharnetzki@gmail.com>
2020-06-16 18:59:04 -0500

    recipes index

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,20 @@
+class RecipesController < ApplicationController
+  def index
+    @recipes = Recipe.all
+  end
+
+  # def show
+  # end
+
+  # def new
+  # end
+
+  # def create
+  # end
+
+  # def edit
+  # end
+
+  # def update
+  # end
+end
---
+++ app/models/recipe.rb
@@ -0,0 +1,13 @@
+class Recipe < ApplicationRecord
+end
+
+# == Schema Information
+#
+# Table name: recipes
+#
+#  id         :bigint           not null, primary key
+#  link       :string
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
