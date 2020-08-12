ec1b3f3aeea006a28cfbcb54830cf148a41acc2b
meara <mcharnetzki@gmail.com>
2020-08-12 13:59:28 -0500

    standardrb cleanup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,5 +11,4 @@ class ApplicationController < ActionController::Base
     flash[:alert] = "You are not authorized to perform this action."
     redirect_to(request.referrer || root_path)
   end
-
 end
